cf3357dd3455312b1a2ba5ee5dae74488388a88c
ireshanwaiman94 <ireshanwaiman@gmail.com>
2025-02-26 23:02:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base:"/ireshan-portfolio/",
+  base:"/ireshan-portfolio/",
   server: {
     hmr: {
       overlay: false,  // Disable error overlay
